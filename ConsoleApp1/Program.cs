0b1b9ca8c695de6df778b91f2ff10b3314351294
MayXoc <mayankbisht8126@gmail.com>
2024-02-02 01:22:16 +0530

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace Sum {
+
+  class Prog
+    {
+        public void SuM(int a, int b)
+        {
+            int c = a + b;
+            Console.WriteLine("Sum of {0} and {1} is {2}",a,b,c);
+        }
+        public static void Main(string[] args)
+        {
+            int a, b;
+            a= Convert.ToInt32(Console.ReadLine());
+            b= Convert.ToInt32(Console.ReadLine());
+            Prog obj = new Prog();
+            obj.SuM(a,b);
+        }
+    }    
+}
