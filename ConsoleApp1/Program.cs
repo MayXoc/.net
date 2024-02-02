946c959db10e4e390dbbe58077f44161a9a336bc
Mayank Bisht <156527603+MayXoc@users.noreply.github.com>
2024-02-02 17:45:31 +0530

    Update Program.cs

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,20 +1,45 @@
 ﻿using System;
 namespace Sum {
+ //Adding Two numbers using function
+  
+  // class Prog
+  //   {
+  //       public void SuM(int a, int b)
+  //       {
+  //           int c = a + b;
+  //           Console.WriteLine("Sum of {0} and {1} is {2}",a,b,c);
+  //       }
+  //       public static void Main(string[] args)
+  //       {
+  //           int a, b;
+  //           a= Convert.ToInt32(Console.ReadLine());
+  //           b= Convert.ToInt32(Console.ReadLine());
+  //           Prog obj = new Prog();
+  //           obj.SuM(a,b);
+  //       }
+  //   } 
 
-  class Prog
-    {
-        public void SuM(int a, int b)
-        {
-            int c = a + b;
-            Console.WriteLine("Sum of {0} and {1} is {2}",a,b,c);
-        }
-        public static void Main(string[] args)
-        {
-            int a, b;
-            a= Convert.ToInt32(Console.ReadLine());
-            b= Convert.ToInt32(Console.ReadLine());
-            Prog obj = new Prog();
-            obj.SuM(a,b);
-        }
-    }    
+  //Factorial function using recursion
+  
+//       public long Factorial(int n)
+//     {
+//         if (n >= 1)
+//         {
+//             return n * Factorial(n - 1);
+//         }
+           
+//         else
+//             return 1;
+//     }
+//     static void Main(string[] args)
+//     {
+//         Program obj = new Program();
+//         int num;
+//         Console.WriteLine("Enter your desired number - ");
+//         num = Convert.ToInt32(Console.ReadLine());
+//         Console.WriteLine("The factorial of {0} is {1}", num, obj.Factorial(num));
+//     }
+// }
+
+  
 }
