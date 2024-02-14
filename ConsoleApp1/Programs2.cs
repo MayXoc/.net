da4a6087659fe36532c19ce10de99231d054dcc2
MayXoc <mayankbisht8126@gmail.com>
2024-02-14 15:36:49 +0530

    Downloading google main page

--- ConsoleApp1/Programs2.cs
+++ ConsoleApp1/Programs2.cs
@@ -1,8 +1,17 @@
 ﻿using System;
+using System.Net;
 namespace Prog
 {
     class program
     {
+
+        //****   Downloading google main page
+       
+         using(WebClient client = new WebClient())
+            {
+                string googleMainPage = client.DownloadString("https://www.google.com/");
+    Console.WriteLine(googleMainPage);
+            }
         
     }
 }
