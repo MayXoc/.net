747e127d3627a1d3fd4fa72388fa4838d342274a
MayXoc <mayankbisht8126@gmail.com>
2024-02-15 22:34:45 +0530

    NULL Coalesing

--- ConsoleApp1/Programs2.cs
+++ ConsoleApp1/Programs2.cs
@@ -1,18 +1,31 @@
 ﻿using System;
 using System.Net;
-
 namespace Prog
 {
-    class Program
+    public class Program
+
     {
+        public class person
+        {
+            public person(string fname, string lname)
+            {
+                firstname = fname;
+                lastname = lname;
+            }
+            public string firstname { get; }
+            public string lastname { get; }
+        }
         public static void Main(string[] args)
         {
-            //**** Downloading Google main page 
-            using (WebClient client = new WebClient())
-            {
-                string googleMainPage = client.DownloadString("https://www.google.com/");
-                Console.WriteLine(googleMainPage);
-            }
+            /*  //**** Downloading Google main page 
+              using (WebClient client = new WebClient())
+              {
+                  string googleMainPage = client.DownloadString("https://www.google.com/");
+                  Console.WriteLine(googleMainPage);
+              }*/
+            person x = null;
+            person y = x ?? new person("Default","person");
+            Console.WriteLine(y.firstname);
         }
     }
 }
