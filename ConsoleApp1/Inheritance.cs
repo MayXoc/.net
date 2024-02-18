bbeb4cd8c448eb822aacb5666f92010ea7fa035b
MayXoc <mayankbisht8126@gmail.com>
2024-02-18 13:52:38 +0530

    Interface class

--- ConsoleApp1/Inheritance.cs
+++ ConsoleApp1/Inheritance.cs
@@ -6,11 +6,31 @@ using System.Threading.Tasks;
 
 namespace ConsoleApp1
 {
-    internal class Inheritance : person
+    internal class Inheritance : person,Interface1
     {
-        
-        public Inheritance() {
-            Console.WriteLine("This is derived class");
+        private int Id;
+        private string Name = string.Empty;
+        private int Salary;
+
+        public void employ(string name, int id, int salary)
+        {
+            if (id < 0)
+                throw new ArgumentException("Id cannot be negative.");
+            if (name == null)
+                throw new ArgumentNullException(nameof(name), "Name cannot be null.");
+            if (salary <= 0)
+                throw new ArgumentException("Salary must be greater than zero.");
+
+            Id = id;
+            Name = name;
+            Salary = salary;
         }
+
+        public void display()
+        {
+            Console.WriteLine("Employee name = {0}, Id = {1}, and Salary = {2}.", Name, Id, Salary);
+        }
+
+       
     }
 }
