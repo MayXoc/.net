e903dbd55e3066e3e85225337a48830d1c38091e
MayXoc <mayankbisht8126@gmail.com>
2024-02-18 14:47:58 +0530

    Interface

---
+++ ConsoleApp1/Interface1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    interface Interface1
+    {
+        void employ(string name,int id,int sallary);
+        void display();
+    }
+}
