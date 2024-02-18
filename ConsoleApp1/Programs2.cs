4af431e6f073b4bf782f0a0c7bd0469b7f26c7a3
MayXoc <mayankbisht8126@gmail.com>
2024-02-18 14:45:54 +0530

    Library implemented

--- ConsoleApp1/Programs2.cs
+++ ConsoleApp1/Programs2.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Net;
 using System.Threading.Tasks;
 using ConsoleApp1;
+using Coding.Exercise;
 namespace Prog
 {
     public class Program
@@ -28,6 +29,25 @@ namespace Prog
             Console.WriteLine(x.Lastname);
             x.employ("mayank", 1, 1_00_000);
             x.display();
+            // Create an instance of PersonalDigitalLibrary
+            PersonalDigitalLibrary myLibrary = new PersonalDigitalLibrary();
+
+            // Create some book instances
+            Book book1 = new Book("The boy who loved", "Durjoy datta ", "4456543");
+            Book book2 = new Book("Perfect us", "Durjoy datta","4657680");
+            Book book3 = new Book("Wings of fire", "APJ","4457672");
+
+            // Add the books to the library
+            myLibrary.AddBook(book1);
+            myLibrary.AddBook(book2);
+            myLibrary.AddBook(book3);
+
+            // Display the books in the library
+            Console.WriteLine("Books in the library:");
+            foreach (Book book in myLibrary.Books)
+            {
+                Console.WriteLine($"Title: {book.Title}, Author: {book.Author}");
+            }
 
 
         }
