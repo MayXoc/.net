5912cbb0b66b9e9fabdaeb67557cda4eff3e4ac6
MayXoc <mayankbisht8126@gmail.com>
2024-02-18 14:39:12 +0530

    Personal Digital Library

---
+++ ConsoleApp1/Book.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Coding.Exercise
+{
+    // Create a new class. (Lesson: Objects & Classes)
+    public class Book
+    {
+        public string Title { get; }
+        public string Author { get; }
+        public string ISBN { get; }
+
+        // Create a constructor and use defensive programming to check all values. (Lesson: Constructors)
+        public Book(string title, string author, string isbn)
+        {
+            if (string.IsNullOrEmpty(title))
+            {
+                throw new ArgumentException("Title is required.");
+            }
+
+            if (string.IsNullOrEmpty(author))
+            {
+                throw new ArgumentException("Author is required.");
+            }
+
+            if (string.IsNullOrEmpty(isbn))
+            {
+                throw new ArgumentException("ISBN is required.");
+            }
+
+            Title = title;
+            Author = author;
+            ISBN = isbn;
+        }
+    }
+}
---
+++ ConsoleApp1/PersonalDigitalLibrary.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Coding.Exercise
+{
+    // Create a new class. (Lesson: Objects & Classes)
+    public class PersonalDigitalLibrary
+    {
+        // Create a nested class within the current class and make it a list.
+        public List<Book> Books { get; set; }
+
+        public PersonalDigitalLibrary()
+        {
+            Books = new List<Book>();
+        }
+
+        // Create a method on your class to accept new books. (Lesson: Methods)
+        public void AddBook(Book book)
+        {
+            // Make sure to use defensive programming to keep out nulls
+            if (book is null)
+            {
+                throw new ArgumentNullException();
+            }
+
+            Books.Add(book);
+        }
+    }
+}
