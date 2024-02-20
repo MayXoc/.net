0da8596d908d1d6c797622190881aee8cfabcd60
MayXoc <mayankbisht8126@gmail.com>
2024-02-20 16:01:56 +0530

    Using (_) to ignore myGas variable in deconstruct function

--- ConsoleApp1/Deconstruct.cs
+++ ConsoleApp1/Deconstruct.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Coding.Exercise;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,24 +12,29 @@ namespace ConsoleApp1
         public void run()
         {
             Console.WriteLine("\n Deconstruct ****\n");
-            var (myGas, myMileage) = new Car1(100, 1000);
-            Console.WriteLine($"My total gas {myGas} and my Mileage {myMileage}");
+            var (myGas, myMileage,CarName) = new Car1(100, 1000,"Jaguar");
+            Console.WriteLine($"Car name {CarName} My total gas {myGas} and my Mileage {myMileage}");
+            var (_, mymileage,carName) = new Car1(100, 1000, "ferarri");// using (_) to ignore the gas 
+            Console.WriteLine($"Car name {carName}  and my Mileage {mymileage}");
         }
     }
     class Car1
     {
         public int gas { get; set;}
         public int mileage { get; set;}
+        public string carName {  get; set;}
 
-        public Car1(int gas, int mileage)
+        public Car1(int gas, int mileage, string carName)
         {
             this.gas = gas;
             this.mileage = mileage;
+            this.carName = carName;
         }
-        public void Deconstruct(out int gas,out int mileage) {
+        public void Deconstruct(out int gas,out int mileage,out string carName) {
          
             gas = this.gas;
             mileage =   this.mileage;
+            carName = this.carName;
         }
     }
 }
