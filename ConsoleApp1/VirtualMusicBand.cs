9519d70227dfb8fb1baa3394c226d2ac2e57a61e
MayXoc <mayankbisht8126@gmail.com>
2024-02-21 23:50:51 +0530

    Creating Virtual Music Box

---
+++ ConsoleApp1/VirtualMusicBand.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+
+namespace Coding.Exercise
+{
+    public class VirtualMusicBand
+    {
+        private List<Instrument> instruments;
+
+        public VirtualMusicBand(List<Instrument> instruments)
+        {
+            this.instruments = instruments;
+        }
+
+        public void PlayAll()
+        {
+            foreach (Instrument instrument in instruments)
+            {
+                // Calling .Play() on each instrument is an example of polymorphism. 
+                string currentInstrument = instrument.Play();
+                int currentInstrumentVolume = instrument.GetVolume();
+
+                Console.WriteLine($"Current instrument: {currentInstrument} | Volume: {currentInstrumentVolume}.");
+            }
+        }
+    }
+}
