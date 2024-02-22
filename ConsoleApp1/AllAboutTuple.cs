d72c0f2b5735bab6ab41be3e0106730cf3321eb6
MayXoc <mayankbisht8126@gmail.com>
2024-02-22 22:11:12 +0530

    Old way of creating tuple

---
+++ ConsoleApp1/AllAboutTuple.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class allTuple 
+    {
+        public void run()
+        {
+            //old way of creating tuple
+            Tuple<string, string, int> oldtuple = getoldtuple();
+            Console.WriteLine($"{oldtuple.Item1} {oldtuple.Item2}");
+        }
+        public Tuple<string, string, int> getoldtuple()
+        {
+            return Tuple.Create("Mayank", "Bisht", 21);
+        }
+    }
+
+}
