c3748ad354f7ea874f7b1a68e5bda129f1e6f76f
MayXoc <mayankbisht8126@gmail.com>
2024-02-23 22:51:41 +0530

    Using Query method

--- ConsoleApp1/AllAboutLinq.cs
+++ ConsoleApp1/AllAboutLinq.cs
@@ -13,9 +13,9 @@ namespace ConsoleApp1
             //making array of hobbies
             Hobbies[] hobbies = { new Hobbies(1, "Games"), new Hobbies(2, "Batminton"), new Hobbies(3, "Music") };
             // now its time to access these hobbies
-            //By using LINQ and Lembda Expression
-            var hobbie = hobbies.Where(h => h.Name.Equals("Games"));    
-
+            //By using LINQ and Lambda Expression
+            var hobbie = hobbies.Where(h => h.Name.Equals("Games"));
+            var anotherHobbie = from h in hobbies where h.Name.Equals("Games") select h;//using Query method both give the same output 
         }
 
     }
