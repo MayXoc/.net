2954830b0b338627a21c89fde728ead1335a1bd0
MayXoc <mayankbisht8126@gmail.com>
2024-02-25 12:41:03 +0530

    Creating new Main Program file since old one got so much to do

---
+++ ConsoleApp1/Program3.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program3
+    {
+        public static void Main(string[] args)
+        {
+            try
+            {
+                InputConverter inputconverter = new InputConverter();
+                CalculatorEngine Calculate = new CalculatorEngine();
+
+                double FirstNumber = inputconverter.ConvertIntoNumeric(Console.ReadLine());
+                double SecondNumber = inputconverter.ConvertIntoNumeric(Console.ReadLine());
+                string Operator = Console.ReadLine();
+
+                double result = Calculate.calculate(Operator, FirstNumber, SecondNumber);
+                Console.WriteLine(result);
+            }
+            catch(Exception e) 
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
