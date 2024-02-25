390a42716755a023de2362e5995d82339eff4550
MayXoc <mayankbisht8126@gmail.com>
2024-02-25 12:45:23 +0530

    Some updated programs

--- ConsoleApp1/NestedFunction.cs
+++ ConsoleApp1/NestedFunction.cs
@@ -6,14 +6,16 @@ using System.Threading.Tasks;
 
 namespace ConsoleApp1
 {
-    internal class NestedFunction
+    public class NestedFunction
     {
         public void HH()
         {
             int a = 10;
             void GG()
             {
+                var a = 50;//child cant override the parent function
                 a = 20;
+                Console.WriteLine(a);
             }
             GG();
             Console.WriteLine($"Can access variable of HH function,value of a after change in function GG - {a}");
--- ConsoleApp1/TaskManagement.cs
+++ ConsoleApp1/TaskManagement.cs
@@ -1,12 +1,53 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace ConsoleApp1
+namespace Coding.Exercise
 {
-    internal class TaskManagement
+    public class TaskManagement
     {
+        private List<(int id, string title, DateTime dueDate, TaskStatus status)> _tasks;
+
+        public TaskManagement()
+        {
+            _tasks = new List<(int, string, DateTime, TaskStatus)>();
+        }
+
+        public void AddTask(int id, string title, DateTime dueDate, TaskStatus status)
+        {
+            _tasks.Add((id, title, dueDate, status));
+        }
+
+        public int TaskCount()
+        {
+            return _tasks.Count();
+        }
+
+        public bool ChangeTaskStatus(int id, TaskStatus status)
+        {
+            for (int i = 0; i < _tasks.Count; i++)
+            {
+                if (_tasks[i].id == id)
+                {
+                    var task = _tasks[i];
+                    _tasks[i] = (task.id, task.title, task.dueDate, status);
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        // Returning back a tuple. (Lesson: Tuples)
+        public (int id, string title, DateTime dueDate, TaskStatus status)? FindTaskById(int id)
+        {
+            return _tasks.Any(task => task.id == id) ? _tasks.First(task => task.id == id) : null;
+        }
+
+        // Using LINQ to get tasks due today or find them by their ID. (Lesson: LINQ Introduction)
+        public List<(int id, string title, DateTime dueDate, TaskStatus status)> GetTasksDueToday()
+        {
+            return _tasks.Where(task => task.dueDate.Date == DateTime.Now.Date).OrderBy(task => task.dueDate).ToList();
+        }
     }
 }
